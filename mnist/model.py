78b65e9bb763da141e843262ada07ea2b96e2107
gzdaijie <gzdaijie@163.com>
2017-12-08 00:39:20 +0800

    init project

---
+++ mnist/model.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+class Network:
+    def __init__(self):
+        # 输入张量 28 * 28 = 784个像素的图片一维向量
+        self.x = tf.placeholder(tf.float32, [None, 784])
+        # 权重，初始化全 0
+        self.w = tf.Variable(tf.zeros([784, 10]))
+        # 偏置 bias， 初始化全 0
+        self.b = tf.Variable(tf.zeros([10]))
+        # 输出
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.w) + self.b)
+
+        # 以下代码训练使用
+
+        # 学习速率，一般在 0.00001 - 0.5 之间
+        self.learning_rate = 0.001
+
+        # 标签值，即图像对应的结果，如果对应数字是8，则对应label是 [0,0,0,0,0,0,0,0,1,0]
+        # 这种方式称为 one-hot编码
+        # 标签是一个长度为10的一维向量，值最大的下标即图片上写的数字
+        self.label = tf.placeholder(tf.float32, [None, 10])
+
+        # 损失，即交叉熵，最常用的计算标签(label)与输出(y)之间差别的方法
+        self.loss = -tf.reduce_sum(self.label * tf.log(self.y + 1e-10))
+
+        # 反向传播，采用梯度下降的方法。调整w与b，使得损失(loss)最小
+        # loss越小，那么计算出来的y值与 标签(label)值越接近，准确率越高
+        self.train = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
+
+        # 以下代码验证正确率时使用
+
+        # argmax 返回最大值的下标，最大值的下标即答案
+        # 例如 [0,0,0,0.9,0,0.1,0,0,0,0] 代表数字3
+        predict = tf.equal(tf.argmax(self.label, 1), tf.argmax(self.y, 1))
+
+        # predict -> [true, true, true, false, false, true]
+        # reduce_mean即求predict的平均数 即 正确个数 / 总数，即正确率
+        self.accuracy = tf.reduce_mean(tf.cast(predict, "float"))
---
+++ mnist/train.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from model import Network
+
+class Train:
+    def __init__(self):
+        self.net = Network()
+
+        # 初始化 session
+        # Network() 只是构造了一张计算图，计算需要放到会话(session)中
+        self.sess = tf.Session()
+        # 初始化变量
+        self.sess.run(tf.global_variables_initializer())
+
+        # 读取训练和测试数据，这是tensorflow库自带的，不存在训练集会自动下载
+        # 项目目录下已经下载好，删掉后，重新运行代码会自动下载
+        # data_set/train-images-idx3-ubyte.gz
+        # data_set/train-labels-idx1-ubyte.gz
+        # data_set/t10k-images-idx3-ubyte.gz
+        # data_set/t10k-labels-idx1-ubyte.gz
+        self.data = input_data.read_data_sets('data_set', one_hot=True)
+
+    def train(self):
+        # batch_size 是指每次迭代训练，传入训练的图片张数。
+        # 数据集小，可以使用全数据集，数据大的情况下，为了提高训练速度，用随机抽取的n张图片来训练，效果与全数据集相近
+        # https://www.zhihu.com/question/32673260
+        batch_size = 64
+
+        # 总的训练次数
+        train_step = 2000
+
+        # 开始训练
+        for i in range(train_step):
+            # 从数据集中获取 输入和标签(也就是答案)
+            x, label = self.data.train.next_batch(batch_size)
+            # 每次计算train，更新整个网络
+            # loss只是为了看到损失的大小，方便打印
+            _, loss = self.sess.run([self.net.train, self.net.loss], feed_dict={self.net.x: x, self.net.label: label})
+
+            # 打印 loss，训练过程中将会看到，loss有变小的趋势
+            # 代表随着训练的进行，网络识别图像的能力提高
+            # 但是由于网络规模较小，后期没有明显下降，而是有明显波动
+            if (i + 1) % 10 == 0:
+                print('第%5d步，当前loss：%.2f' % (i + 1, loss))
+
+
+    def calculate_accuray(self):
+        test_x = self.data.test.images
+        test_label = self.data.test.labels
+        # 注意：与训练不同的是，并没有计算 self.net.train
+        # 只计算了accuracy这个张量，所以不会更新网络
+        # 最终准确率约为0.91
+        accuracy = self.sess.run(self.net.accuracy, feed_dict={self.net.x: test_x, self.net.label: test_label})
+        print("准确率: %.2f，共测试了%d张图片 " % (accuracy, len(test_label)))
+
+
+if __name__ == "__main__":
+    app = Train()
+    app.train()
+    app.calculate_accuray()
