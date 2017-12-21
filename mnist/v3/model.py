49947c3c256052054d5af05c5d32f22ee9cd96d0
gzdaijie <gzdaijie@163.com>
2017-12-22 01:29:37 +0800

    添加变量的name属性

--- mnist/v3/model.py
+++ mnist/v3/model.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 class Network:
     def __init__(self):
         self.learning_rate = 0.001
-        self.global_step = tf.Variable(0, trainable=False)
+        self.global_step = tf.Variable(0, trainable=False, name="global_step")
 
-        self.x = tf.placeholder(tf.float32, [None, 784])
-        self.label = tf.placeholder(tf.float32, [None, 10])
+        self.x = tf.placeholder(tf.float32, [None, 784], name="x")
+        self.label = tf.placeholder(tf.float32, [None, 10], name="label")
 
-        self.w = tf.Variable(tf.zeros([784, 10]))
-        self.b = tf.Variable(tf.zeros([10]))
-        self.y = tf.nn.softmax(tf.matmul(self.x, self.w) + self.b)
+        self.w = tf.Variable(tf.zeros([784, 10]), name="fc/weight")
+        self.b = tf.Variable(tf.zeros([10]), name="fc/bias")
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.w) + self.b, name="y")
 
         self.loss = -tf.reduce_sum(self.label * tf.log(self.y + 1e-10))
         self.train = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(
@@ -20,8 +20,9 @@ class Network:
         predict = tf.equal(tf.argmax(self.label, 1), tf.argmax(self.y, 1))
         self.accuracy = tf.reduce_mean(tf.cast(predict, "float"))
 
-        # loss 和 accuracy画折线图
-        # w 画直方图
-        # tf.summary.histogram('weights', self.w)
+        # w, b 画直方图
+        # loss, accuracy画标量图
+        tf.summary.histogram('weight', self.w)
+        tf.summary.histogram('bias', self.b)
         tf.summary.scalar('loss', self.loss)
         tf.summary.scalar('accuracy', self.accuracy)
